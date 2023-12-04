cc3f9b01c97ad2c652fb87a64ad795e477036e00
GabyOka <102690505+GabyOka@users.noreply.github.com>
2023-12-04 16:14:29 -0300

    commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function playNav(){
+    alert("Escolha um bloco de conteúdo para direcionar a pesquisa")
+}
+
+function openNav(){
+alert("Reporte erro através da sessão avalie-nos indicado pelo ícone pessoa")
+}
